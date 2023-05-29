4d45fe06b3416610afefaff06a5a73c25cabc2e3
BobbyPunnawich <bobbypunnawich@gmail.com>
2023-05-29 21:21:24 +0700

    Update Basic

---
+++ Basic/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Bobby");
+console.log("Hello Bobby");
+console.log("Hello Joey");
+
+alert("Bobby");
+alert("Joey");
+
+prompt("Hi");
+
+var name = "Bobby";
+console.log(name);
+name = "Killer";
+console.log(name);
