67f5972b2b1f755113648b0d7b0bac9de243e082
BobbyPunnawich <bobbypunnawich@gmail.com>
2023-05-29 20:37:27 +0700

    delete old folder

--- project1/script.js
+++
@@ -1,5 +0,0 @@
-let num = prompt("enter your lucky number");
-console.log(num);
-document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
-document.getElementById("lucky").innerHTML = num;
-
